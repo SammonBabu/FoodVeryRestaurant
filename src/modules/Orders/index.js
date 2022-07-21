96723ca750065d3cae1b8cf3b35bae2691f8ab6d
Sammon Babu <sambmon@gmail.com>
2022-07-21 15:25:14 +0530

    handling errors

--- src/modules/Orders/index.js
+++ src/modules/Orders/index.js
@@ -37,7 +37,7 @@ const Orders = () => {
       }
     });
     return () => subscription.unsubscribe();
-  }, []);
+  }, [restaurant]);
 
   const renderOrderStatus = (orderStatus) => {
     const statusToColor = {
